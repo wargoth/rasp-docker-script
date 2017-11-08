0b530aa237e4d76323c2ef60a8804243ebc67853
Valentin Mayamsin <yavalek@gmail.com>
2017-11-07 17:57:18 -0800

    Extend Panoche forecast

--- www/corners.js
+++ www/corners.js
@@ -49,7 +49,7 @@ var f = {
     'centre': corners.Centre[4],
 }
 forecasts.push(f)
-    
+
 T.setTime(Now + mS_Day * 2);
 var f = {
     'name': dayName[T.getDay()] + ' ' + T.getDate() + ' ' + monthName[T.getMonth()],
@@ -57,6 +57,42 @@ var f = {
     'date': T.getTime(),
     'default_t': '1400',
     'dir': 'OUT+2/FCST/',
+    'bounds': corners.Bounds[4],
+    'centre': corners.Centre[4],
+}
+forecasts.push(f)
+
+T.setTime(Now + mS_Day * 3);
+var f = {
+    'name': dayName[T.getDay()] + ' ' + T.getDate() + ' ' + monthName[T.getMonth()],
+    'latlon_file': 'latlon2d.json',
+    'date': T.getTime(),
+    'default_t': '1400',
+    'dir': 'OUT+3/FCST/',
+    'bounds': corners.Bounds[4],
+    'centre': corners.Centre[4],
+}
+forecasts.push(f)
+
+T.setTime(Now + mS_Day * 4);
+var f = {
+    'name': dayName[T.getDay()] + ' ' + T.getDate() + ' ' + monthName[T.getMonth()],
+    'latlon_file': 'latlon2d.json',
+    'date': T.getTime(),
+    'default_t': '1400',
+    'dir': 'OUT+4/FCST/',
+    'bounds': corners.Bounds[4],
+    'centre': corners.Centre[4],
+}
+forecasts.push(f)
+
+T.setTime(Now + mS_Day * 5);
+var f = {
+    'name': dayName[T.getDay()] + ' ' + T.getDate() + ' ' + monthName[T.getMonth()],
+    'latlon_file': 'latlon2d.json',
+    'date': T.getTime(),
+    'default_t': '1400',
+    'dir': 'OUT+5/FCST/',
     'bounds': corners.Bounds[4],
     'centre': corners.Centre[4],
 }
